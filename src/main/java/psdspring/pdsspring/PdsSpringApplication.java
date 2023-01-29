9aa53f81c35f2c9ec7e3d1932f4ec58139cce3b4
azsx92 <azsx92@gmail.com>
2023-01-29 23:56:50 +0900

    feat: pr test

---
+++ src/main/java/psdspring/pdsspring/PdsSpringApplication.java
@@ -0,0 +1,13 @@
+package psdspring.pdsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsSpringApplication.class, args);
+	}
+
+}
